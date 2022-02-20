d64c4b8202474ab118674fc3e43c8da76094633a
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-02-21 02:01:48 +0530

    Time: 17 ms (36.40%), Space: 44.6 MB (17.42%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -3,6 +3,8 @@ class Solution {
         int count=0;
         
         //First of all we create a 2-D boolean DP array and fill true on all the pallindromic substrings.
+        
+        //This array is filled using "Gap Strategy" or diagonal traversal.
         boolean[][] dp1 = new boolean[s.length()][s.length()];
         for (int g = 0; g < dp1.length; g++) {
             for (int i = 0, j = g; j < dp1.length; i++, j++) {
