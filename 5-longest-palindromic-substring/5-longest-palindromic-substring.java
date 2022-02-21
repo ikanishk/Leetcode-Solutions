792fb4a62a8e33845945d9004a252aba53bd4d1b
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-02-21 11:45:46 +0530

    Time: 254 ms (33.13%), Space: 46.5 MB (38.79%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,15 +1,17 @@
 class Solution {
     public String longestPalindrome(String s) {
+        // Fill a boolean DP array using "Gap Strategy" or "Diagonal Traversal".
+        
         boolean[][] dp1 = new boolean[s.length()][s.length()];
         int longg=0;
         int li=0;
-            int ri=0;
+        int ri=0;
         for (int g = 0; g < dp1.length; g++) {
             for (int i = 0, j = g; j < dp1.length; i++, j++) {
                 
                 if (g==0) {
                     dp1[i][j] = true;
-                    longg=g+1;
+                    longg=g+1; //whenever we encounter a true(pallindromic substring), we store it's gap size and i,j in a global variable and then change it only when we find a bigger gap(longer substring).
                     li=i;
                     ri=j;
                 }
