efdf022601c9f05cf1cdfe6d556b3b1fd36a5abd
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-02-22 01:15:35 +0530

    Time: 1 ms (100.00%), Space: 73.8 MB (58.91%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,10 +1,34 @@
 class Solution {
-    public int maxSubArray(int[] nums) {
-        int pre = 0, maxAns = nums[0];
-        for (int num : nums) {
-            pre = Math.max(pre + num, num);
-            maxAns = Math.max(maxAns, pre);
-        }
-        return maxAns;
+    //Kadane's algorithm
+    public int maxSubArray(int[] arr) {
+      int csum=arr[0];
+      int bsum=arr[0];
+        
+    for(int i=1;i<arr.length;i++){
+    if(csum>0){
+    csum+=arr[i];
+    }
+    else{
+    csum=arr[i];
+    }
+    
+    if(csum>bsum){
+        bsum=csum;
+    }
+    }
+    return bsum;
     }
 }
+
+
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int pre = 0, maxAns = nums[0];
+//         for (int num : nums) {
+//             pre = Math.max(pre + num, num);
+//             maxAns = Math.max(maxAns, pre);
+//         }
+//         return maxAns;
+//     }
+// }
