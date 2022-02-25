da9c78dcc82635c3b778a930d4c9560dbb705334
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-02-25 18:51:35 +0530

    Time: 2 ms (61.07%), Space: 41.3 MB (22.07%) - LeetHub

--- 263-ugly-number/263-ugly-number.java
+++ 263-ugly-number/263-ugly-number.java
@@ -1,6 +1,9 @@
 class Solution {
  public boolean isUgly(int n) {
-        if(n == 0) return false;
+        if(n == 0) {
+        return false;
+   
+        }
         while(n % 2 == 0) n /= 2; // divide untill divisible by 2
         while(n % 3 == 0) n /= 3; // divide untill divisible by 3
         while(n % 5 == 0) n /= 5; // divide untill divisible by 5
