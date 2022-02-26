0cf6fa9fc5862adc47bcbf808064d86716caf45a
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-02-26 11:45:46 +0530

    Time: 695 ms (32.42%), Space: 110.7 MB (20.12%) - LeetHub

--- 313-super-ugly-number/313-super-ugly-number.java
+++ 313-super-ugly-number/313-super-ugly-number.java
@@ -1,27 +1,53 @@
 class Solution {
-    public int nthSuperUglyNumber(int n, int[] primes) {
-    int[]pointers=new int[primes.length];
-    int[]dp=new int[n+1];
     
-    dp[1]=1;
+    
+        public static class Pair implements Comparable<Pair>{
+        int prime;
+        int ptr;
+        int val;
         
-    for(int i=0;i<pointers.length;i++){
-    pointers[i]=1;
-    }
+        Pair() {
+            
+        }
         
-    for(int i=2;i<=n;i++){
-    Integer[]product=new Integer[pointers.length];
-    for(int j=0;j<product.length;j++){
-    product[j]=primes[j]*dp[pointers[j]];
-    }
-    int min = Collections.min(Arrays.asList(product));
-    dp[i]=min;
-        for(int j=0;j<product.length;j++){
-        if(product[j]==min){
-        pointers[j]++;
+        Pair(int prime,int ptr,int val) {
+            this.prime = prime;
+            this.ptr = ptr;
+            this.val = val;
         }
+        
+        public int compareTo(Pair o) {
+            return this.val - o.val;
         }
+        
     }
+    
+    public static int nthSuperUglyNumber(int n, int[] primes) {
+        int[]dp = new int[n+1];
+        PriorityQueue<Pair>pq = new PriorityQueue<>();
+        
+        for(int i=0; i < primes.length;i++) {
+            Pair p = new Pair(primes[i],1,primes[i]*1);
+            pq.add(p);
+        }
+        
+        dp[1] = 1;
+        
+        for(int i=2; i <= n;) {
+            Pair top = pq.remove();
+            
+            if(dp[i-1] != top.val) {
+                dp[i] = top.val;
+                i++;
+            }
+            
+            int np = top.ptr + 1;
+            int nv = (top.prime)*(dp[top.ptr]);
+            
+            pq.add(new Pair(top.prime,np,nv));
+            
+        }
+        
         return dp[n];
     }
 }
