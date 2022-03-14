4745241aedafb6e2d5e2c6b86cd5859a6c6e1dd9
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-03-15 00:37:04 +0530

    Time: 1 ms (83.76%), Space: 42.1 MB (30.25%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.java
+++ 925-long-pressed-name/925-long-pressed-name.java
@@ -1,43 +1,26 @@
 class Solution {
-public boolean isLongPressedName(String name, String typed) {
-int i = 0;
-int j = 0;
-char temp = ' ';
-
-    outer:
-    while (i<name.length() || j<typed.length()){
-        // If typed has reached its end.
-        if (j==typed.length()){
-            break outer;
-        }
-        // name has reached its end but we still need to compare the characters of typed string.
-        if(i==name.length()){
-            if(typed.charAt(j)==temp){
-                j++;
-            }else{
-                return false;
-            }
-        }
-        // If both characters match increment position in both strings i.e. name and typed.
-        else if(name.charAt(i)==typed.charAt(j)){
-            temp = name.charAt(i);
+    public boolean isLongPressedName(String name, String typed) {
+    if(typed.length()<name.length()){
+        return false;
+    }
+     int i=0;
+     int j=0;
+     
+    while( j<typed.length()){
+        if(i<name.length() &&(name.charAt(i)==typed.charAt(j))){
             i++;
             j++;
         }
-        // Checking if user long pressed a character.
-        else if(typed.charAt(j)==temp){
+        
+        else if(j>0 && typed.charAt(j)==typed.charAt(j-1)){
             j++;
-        }else{
-            break outer;
+        }
+        
+        else{
+         return false;   
         }
         
     }
-    
-    // If we reached end of name, means there was no mismatch in characters and considering long pressed is not a mismatch.
-    if (i==name.length()){
-        return true;
-    }else{
-        return false;
+    return i==name.length();
     }
-}
 }
