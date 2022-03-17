140619161960cbe044e2de3fe2d56e465a5b1beb
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-03-17 11:53:36 +0530

    Time: 1 ms (100.00%), Space: 56.1 MB (16.13%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -2,10 +2,13 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
         int[]arr=new int[nums.length];
         
+        //make two pointers (i and j) at 0 and arr.length-1.
+        
         int i=0;
         int j=nums.length-1;
-        int k=nums.length-1;
+        int k=nums.length-1;// this is the location where the largest of i and j squares will be filled.
 
+        //find the largest square amongst element at i and j.
         while(i<=j){
         if(nums[i]*nums[i]<=nums[j]*nums[j]){
         arr[k]=nums[j]*nums[j];
