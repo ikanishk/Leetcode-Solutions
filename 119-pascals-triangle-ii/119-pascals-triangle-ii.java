0273e76ca786aa32064b0c6a0a98fcb0a67eecbd
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-03-26 11:36:17 +0530

    Time: 1 ms (88.58%), Space: 41.1 MB (74.36%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -4,23 +4,9 @@ class Solution {
         long val=1;
         for(int j=0;j<=i;j++){
         ans.add((int)val);
-        val = (val * (i - j)) / (j + 1);
+        val = (val * (i - j)) / (j + 1); // Learn to fin NcR+1 from NcR.
         }
         return ans;
     }
 }
 
-
-// class Solution {
-//    public List<Integer> getRow(int rowIndex) {
-//         List<Integer> ans = new ArrayList<>();                
-//         long val=1;
-		
-//         for(int j = 0; j <= rowIndex; j++){
-//                 ans.add((int)val );
-//                 val = (val * (rowIndex - j)) / (j + 1);
-//             }
-			
-//         return ans;
-//     }
-// }
