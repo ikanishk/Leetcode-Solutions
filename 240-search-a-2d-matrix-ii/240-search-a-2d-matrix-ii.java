5681b5b6de2523031e962bdf9d6532091a081183
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-04-03 10:58:27 +0530

    Time: 13 ms (30.33%), Space: 58 MB (42.40%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,4 +1,4 @@
-
+//The Approach is that we start searching from the last element of the first row and if the element is less than target, we increase the row(as above that row are all smaller numbers) and if we find a greater number then we decrease the column./
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix.length == 0) {
