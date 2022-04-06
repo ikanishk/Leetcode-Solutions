689fc68e696ab8775bcc7d3c142bf37ccd13462d
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-04-06 10:47:52 +0530

    Added solution - LeetHub

---
+++ Counts Zeros Xor Pairs - GFG/counts-zeros-xor-pairs.java
@@ -0,0 +1,88 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    
+		    Complete obj = new Complete();
+		    long res = obj.calculate(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static long calculate (int arr[], int n) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        // Traverse through array elements and
+        // count frequencies
+        for (int i = 0; i <arr.length; i++)
+        {
+            if (mp.containsKey(arr[i]))
+            {
+                mp.put(arr[i], mp.get(arr[i]) + 1);
+            }
+            else
+            {
+                mp.put(arr[i], 1);
+            }
+        }
+        int count=0;
+        for(int key:mp.keySet()){
+        int val=mp.get(key)-1;
+        count=count+((val*(val+1))/2);
+        }
+        return count;
+    }
+    
+    
+}
+
+
