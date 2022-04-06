dc722ce192b912fc771ffef62234d4f4800908f9
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-04-06 23:37:01 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (22.37%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    //The thought is that the minimum element would be the only one who is less than its previous in a sorted array.
     public int findMin(int[] nums) {
         int min=nums[0];
         for(int i=1;i<nums.length;i++){
