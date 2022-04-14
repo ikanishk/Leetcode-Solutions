b5f7a68d66a462392053299b10b0765bd04ca74e
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-04-14 17:02:45 +0530

    Time: 3 ms (76.50%), Space: 42 MB (80.79%) - LeetHub

--- 781-rabbits-in-forest/781-rabbits-in-forest.java
+++ 781-rabbits-in-forest/781-rabbits-in-forest.java
@@ -2,11 +2,13 @@ class Solution {
  public int numRabbits(int[] answers) {
         HashMap<Integer,Integer>map = new HashMap<>(); //ans vs count of rabbits
         
+     //create a frequency map
         for(int i=0; i < answers.length;i++) {
             int nf = map.getOrDefault(answers[i],0) + 1;
             map.put(answers[i],nf);
         }
         
+     //Run a loop on the keyset of all the values and gather all the rabbits in every group and add them up in mr(main result).//
         int mr = 0;
         
         for(int key : map.keySet()) {
