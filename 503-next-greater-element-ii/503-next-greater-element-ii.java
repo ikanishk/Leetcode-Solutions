4f6ae8ceb5fb3c9b3b6da8217717c4433cd7221a
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-04-22 18:09:22 +0530

    Time: 42 ms (44.89%), Space: 55.4 MB (20.26%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,20 +1,31 @@
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length; 
-        int nge[] = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for(int i = 2*n-1;i>=0;i--) {
-            while(st.isEmpty() == false && st.peek() <= nums[i%n]) {
-                st.pop(); 
+    public int[] nextGreaterElements(int[] arr) {
+        int n = arr.length;
+        int[]ans = new int[n];
+        Stack<Integer>st = new Stack<>();
+        
+        for(int i=n-1; i >= 0;i--) {
+            while(st.size() > 0 && st.peek() <= arr[i]) {
+                st.pop();
             }
-            
-            if(i<n) {
-                if(st.isEmpty() == false) nge[i] = st.peek(); 
-                else nge[i] = -1; 
-            }
-            
-            st.push(nums[i%n]);
+            st.push(arr[i]);
         }
-        return nge; 
+        
+        for(int i=n-1; i >= 0;i--) {
+            while(st.size() > 0 && st.peek() <= arr[i]) {
+                st.pop();
+            }
+            
+            if(st.size() == 0) {
+                ans[i] = -1;
+            }
+            else {
+                ans[i] = st.peek();
+            }
+            
+            st.push(arr[i]);
+        }
+        
+        return ans;
     }
 }
