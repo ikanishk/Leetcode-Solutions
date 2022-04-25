bfee8fe7c19cb774e876d83de42501425672c3fd
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-04-25 14:43:15 +0530

    Time: 2 ms (17.97%), Space: 41.8 MB (65.71%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,56 +1,19 @@
 class Solution {
-    public int scoreOfParentheses(String s) {
-        return helper(s);
-    }
-    
-    public int helper(String s) {
-        //what is the base case -> (), s is always balanced
-        if( s.equals("()") ){
-            return 1;
-        }
-        
-
-        int counter = 1; //counter for finding closing paren
-        String temp; //checks for paren value
-        
-        if(s.length() != 0) {
-            
-            //find the closing paren for first paren
-            for(int i = 1; i < s.length(); i++){
-                temp = s.substring(i, i+1);
-                counter += calcScore(temp);
-                
-                //when the closing paren is found...
-                if(counter == 0){
-                    
-                    //if closing parenthesis isn't in last position
-                    if(i < s.length()-1){ 
-                        //example of A + B
-                        return helper(s.substring(0, i+1)) + helper(s.substring(i+1));
-                        
-                    } else { // example of (A)
-
-                        return 2 * helper(s.substring(1, s.length()-1));
-                    }
-                    
-                }
-                      
-                
+    public int scoreOfParentheses(String S) {
+        int count = 0; 
+        Stack<Integer> st = new Stack<>();
+        for(char c : S.toCharArray()) {
+            int val = 0;
+            if(c=='(') st.push(0);
+            else {
+                //Calculate the score
+                while(st.peek() != 0) val+=st.pop();
+                val = Math.max(2*val,1);   //() gives 1
+                st.pop();
+                st.push(val);
             }
         }
-
-        return 0; //this should never happen!
-        
+        while(!st.isEmpty()) count+=st.pop();
+        return count;
     }
-
-    //calculate whether a character is a '(' or ')'.
-    public int calcScore(String s) {
-        if( s.equals("(") ){
-            return 1;
-        } else { //closing bracket
-            return -1;
-        } 
-            
-    }
-    
 }
