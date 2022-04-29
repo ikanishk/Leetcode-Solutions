f5533b111ce139ed1c7808ea904e0ec4d29336fe
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-04-29 11:02:51 +0530

    Time: 12 ms (55.07%), Space: 48.6 MB (31.86%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -0,0 +1,37 @@
+class Solution {
+public int[] asteroidCollision(int[] ast)
+{
+
+    Stack<Integer> st = new Stack<>();
+    int n=ast.length;
+   for(int i=0;i<n;i++)
+    {
+        if(ast[i]<=0)
+        {
+            while(!st.isEmpty() && st.peek ()> 0 && st.peek()<ast[i]*-1)
+                st.pop();
+            
+            if(!st.isEmpty() && st.peek()==ast[i]*-1)
+                st.pop();
+            
+            else if(!st.isEmpty() && st.peek() >= -1*ast[i])
+                continue;
+            else
+                st.push (ast[i]);
+        }
+       else
+           st.push(ast[i]);
+        
+        
+    }
+    
+    n=st.size();
+    int arr[]=new int[n];
+    
+    for(int i=n-1;i>=0;i--)
+    arr[i]=st.pop();
+    return arr;
+	
+	
+	}
+}
