417a124bd768dceff4bdee303bf2e93beaa4e918
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-05-21 20:21:00 +0530

    Time: 33 ms (37.72%), Space: 57.4 MB (14.45%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int coinChange(int[] c, int a) {
+        int k=c.length;
+        int b[][]=new int[k+1][a+1];
+        for(int i=0;i<=a;i++)
+        {
+            b[0][i]=10001;
+        }
+        for(int i=0;i<=k;i++)
+        {
+            b[i][0]=0;
+        }
+        for(int i=1;i<=k;i++)
+        {
+            for(int j=1;j<=a;j++)
+            {
+                if(c[i-1]>j)
+                {
+                    b[i][j]=b[i-1][j];
+                }
+                else
+                {
+                    b[i][j]=Math.min(b[i-1][j],1+b[i][j-c[i-1]]);
+                }
+            }
+        }
+        return b[k][a]==10001 ? -1 : b[k][a];
+    }
+}
