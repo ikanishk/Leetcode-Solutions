0e998ae613745bb4a65007dd89106fe2d94bc911
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-06-09 01:54:39 +0530

    Time: 0 ms (100.00%), Space: 51.4 MB (24.71%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,12 +1,10 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-    int ex=((nums.length)*(nums.length+1))/2;  //multiply the (length of array) and (length of                                                  array+1).
-    int sum=0;
-        
-    for(int i=0;i<=nums.length-1;i++){
-    sum=sum+nums[i];                         //find out the aggregate sum of the array.
+public int missingNumber(int[] nums) { //xor
+    int res = nums.length;
+    for(int i=0; i<nums.length; i++){
+        res ^= i;
+        res ^= nums[i];
     }
-        
-    return ex-sum;
-    }
+    return res;
+}
 }
