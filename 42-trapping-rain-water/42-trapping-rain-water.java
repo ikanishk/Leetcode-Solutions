44a44b4137613da0f8625782a8753188f92b482d
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-06-16 02:51:23 +0530

    Time: 3 ms (24.11%), Space: 48.8 MB (24.18%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -4,15 +4,20 @@ class Solution {
         if(n<3){
             return 0;
         }
+        
+        //Create 2 Auxillary arrays for the "Prefix Max" and the "Suffix Max".
         int leftMax[] = new int[n];
         int rightMax[] = new int[n];
         leftMax[0] = height[0];
         rightMax[n-1] = height[n-1];
+        
+        //Store the leftMax till 'i' in leftMax[i] and rightMax till 'i' in rightMax[i].
         for(int i=1;i<n;i++){
             leftMax[i] = Math.max(leftMax[i-1], height[i]);
             rightMax[n-i-1] = Math.max(rightMax[n-i], height[n-i-1]);
         }
         
+        //Iterate the array and apply the formula.
         int trappedWater = 0;
         for(int i=0;i<n;i++){
             trappedWater += Math.min(leftMax[i], rightMax[i])-height[i];
