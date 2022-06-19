c1e9d60e9767d9df5438ac8a5c8620edddea8769
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-06-19 14:19:55 +0530

    Time: 1 ms (66.67%), Space: 41.4 MB (33.33%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
@@ -3,11 +3,15 @@ int minimumNumbers(int num, int k) {
         
         if(num==0)return 0;
         
+    //Find the unit digit of the number.
         int un=num%10;
+    
+    //if num is less than k, then it cannot add up, so return -1.
         if(num<k){
             return -1;
         }
         
+    //we multiply k with values of 'i' from 1 -> 10 and check if the unit digits of (k*i) matches num.
         for(int i=1;i<=10;i++){
             int res=(k*i)%10;
             if(res==un){
