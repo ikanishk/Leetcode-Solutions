4f3056ccd8dc8e625194338dbc5507e173318bf7
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-07-07 01:41:32 +0530

    Time: 55 ms (17.84%), Space: 67.5 MB (12.75%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,29 +1,50 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    static class Pair implements Comparable<Pair> {
+        int val;
+        int gap;
         
-        Queue<int[]> queue = new PriorityQueue<>((a,b) -> b[1]!=a[1] ? b[1] - a[1] : a[0] - b[0]);
-		
-        //Defined a priority queue which will always keep the maximum diff element in the top of the 
-		// queue whenever we find a element with diff smaller we will pop the queue and add the current 
-		// element .
-
-        for(int i=0;i<arr.length;i++){
-            if(queue.size() == k){
-                if(Math.abs(arr[i]-x) < queue.peek()[1]){
-                    queue.poll();
-                    queue.add(new int[]{i,Math.abs(x-arr[i])});
-                }
-            }else{
-                queue.add(new int[]{i,Math.abs(x-arr[i])});
-            }
+        Pair() {
             
         }
-        List<Integer> result = new ArrayList();
-        int size = queue.size();
-        for(int i=0;i<size;i++){
-            result.add(arr[queue.poll()[0]]);
+        
+        Pair(int val,int gap) {
+            this.val = val;
+            this.gap = gap;
         }
-        Collections.sort(result);
-        return result;
+        
+        public int compareTo(Pair o) {
+            if(this.gap != o.gap) {
+                return this.gap - o.gap;
+            }
+            else {
+                return this.val - o.val;
+            }
+        }
+    }
+    
+    
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<Pair>pq = new PriorityQueue<>(Collections.reverseOrder()); //max heap
+        
+        for(int i=0; i < arr.length;i++) {
+            if(pq.size() < k) {
+                pq.add(new Pair(arr[i],Math.abs(arr[i]-x)));
+            }
+            else if(pq.peek().gap > Math.abs(arr[i]-x)) {
+                pq.remove();
+                pq.add(new Pair(arr[i],Math.abs(arr[i]-x)));
+            }
+        }
+        
+        List<Integer>ans = new ArrayList<>();
+        
+        while(pq.size() > 0) {
+            Pair p = pq.remove();
+            ans.add(p.val);
+        }
+        
+        Collections.sort(ans);
+        
+        return ans; 
     }
 }
