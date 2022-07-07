76812d7024836f10e0bd81092c41d13935bb4d7e
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-07-08 02:45:56 +0530

    Time: 22 ms (92.64%), Space: 50.3 MB (98.60%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -2,6 +2,7 @@ class Solution {
     public int[][] kClosest(int[][] points, int K) {
         if(K == points.length) return points;
         
+        //create a PQ witha comparator that puts the greates Euclid distance at the top so that we can remove it when the PQ size exceeds k.
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]));
             
        
