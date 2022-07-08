50424aefae7c7102b0f5a6fdb3b43f0a9dd1fb2e
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-07-08 23:50:36 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (19.27%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,21 +1,34 @@
-class Solution {
-    public int rob(int[] nums) {
-    int n=nums.length;
-     int inc = 0;
-     int exc = 0;
+// class Solution {
+//     public int rob(int[] nums) {
+//     int n=nums.length;
+//      int inc = 0;
+//      int exc = 0;
      
-     for(int i=1; i <=n;i++) {
-         int ninc = exc+nums[i-1];
-         int nexc = Math.max(inc,exc);
+//      for(int i=1; i <=n;i++) {
+//          int ninc = exc+nums[i-1];
+//          int nexc = Math.max(inc,exc);
          
-         inc = ninc;
-         exc = nexc;
-     }
+//          inc = ninc;
+//          exc = nexc;
+//      }
      
-     int total = Math.max(inc,exc);
+//      int total = Math.max(inc,exc);
      
      
      
-     return total;
+//      return total;
+//     }
+// }
+
+class Solution {
+public int rob(int[] num) {
+    int rob = 0; //max monney can get if rob current house
+    int notrob = 0; //max money can get if not rob current house
+    for(int i=0; i<num.length; i++) {
+        int currob = notrob + num[i]; //if rob current value, previous house must not be robbed
+        notrob = Math.max(notrob, rob); //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
+        rob = currob;
     }
+    return Math.max(rob, notrob);
+}
 }
