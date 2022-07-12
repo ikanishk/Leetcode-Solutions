fd9bebcc1c2fce93457a0f100522095b2b8ba166
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-07-12 14:50:50 +0530

    Time: 1 ms (91.03%), Space: 43.6 MB (64.51%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,19 +1,27 @@
 class Solution {
-public void nextPermutation(int[] num) {
-    for(int i = num.length-1; i > 0; i--) {
-        if(num[i-1] < num[i]) {
-            Arrays.sort(num, i, num.length);
-            for(int j = i; j < num.length; j++) {
-                if(num[j] > num[i-1]) {
-                   // swap num[i-1] and num[j]
-                    num[i-1] = num[i-1] + num[j];
-                    num[j] = num[i-1] - num[j];
-                    num[i-1] = num[i-1] - num[j];
-                    return;
-                }
-            }
+    public void nextPermutation(int[] nums) {
+        int k=nums.length-2;
+        //Find the breakpoint in reverse traversal.
+        while(k>=0&&nums[k]>=nums[k+1]) k--;
+        if(k<0) reverse(nums,0,nums.length-1);//when the whole array is decreasing, the next perm is the reverse.
+       
+        else{
+            int l=nums.length-1;
+            while(nums[l]<=nums[k]) l--;
+            int temp = nums[l];
+            nums[l] = nums[k];
+            nums[k] = temp;
+            reverse(nums,k+1,nums.length-1);
         }
     }
-    Arrays.sort(num);
-}
+    
+    private void reverse(int[] nums,int l,int r){
+        while(l<r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }   
 }
