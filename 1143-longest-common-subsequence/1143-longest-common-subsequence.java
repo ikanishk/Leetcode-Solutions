e250b83eca0c17a384758dc1f418754b8056b579
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-07-13 00:55:26 +0530

    Time: 16 ms (79.55%), Space: 46.9 MB (13.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,7 +5,8 @@ class Solution {
         for(int i=dp.length-2;i>=0;i--){
             for(int j=dp[i].length-2;j>=0;j--){
              
-                
+                //if both characters match, then add 1 to the diagonal
+                //if they don't match take the max of the immediate right and down.
                 if(s1.charAt(i)==s2.charAt(j)){
                 dp[i][j]=1+dp[i+1][j+1];
                 }
