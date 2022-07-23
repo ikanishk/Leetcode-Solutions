cfa2089b423835baf6695484918a89b9f5ba468a
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-07-24 00:28:33 +0530

    Time: 7 ms (20.00%), Space: 95.8 MB (80.00%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,17 +1,18 @@
 class Solution {
-    public long zeroFilledSubarray(int[] nums) {
-        int n = nums.length;
-        long length = 0;
-        long ans = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                length++;
-                ans+=length;
-            }
-            else
-                length=0;
-        }
-        
-        return ans;
-    }
+	public long zeroFilledSubarray(int[] arr) {
+		long count = 0;
+		long sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				count++;
+			} else {
+				sum += (count * (count + 1) / 2);
+				count = 0;
+			}
+		}
+		if (arr[arr.length - 1] == 0) {
+			sum += (count * (count + 1) / 2);
+		}
+		return sum;
+	}
 }
