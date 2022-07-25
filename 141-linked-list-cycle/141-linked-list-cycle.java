997bed230bba86c2596ec179617716e067667c50
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-07-25 22:59:46 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (58.52%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -9,17 +9,33 @@
  *     }
  * }
  */
-public class Solution {
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//     if(head==null) return false;
+//     HashSet<ListNode> set = new HashSet<>();
+//     while(head!=null){
+//         if(set.contains(head)){
+//             return true;
+//         }
+//         set.add(head);
+//         head=head.next;
+//     }
+//     return false;
+// }
+// }
+
+public class Solution{
     public boolean hasCycle(ListNode head) {
-    if(head==null) return false;
-    HashSet<ListNode> set = new HashSet<>();
-    while(head!=null){
-        if(set.contains(head)){
-            return true;
-        }
-        set.add(head);
-        head=head.next;
+    ListNode slow = head, fast = head;
+  
+    while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    
+    if (slow == fast) 
+        return true;
     }
-    return false;
+  
+  return false;
 }
 }
