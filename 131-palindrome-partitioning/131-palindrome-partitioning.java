d11537c7eeaafb98330a87efd4e6cdd143f514eb
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-08-02 18:47:47 +0530

    Time: 16 ms (73.44%), Space: 191.8 MB (61.58%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,24 +1,28 @@
 public class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<List<String>>();
-        if (s.equals("")) {
-            res.add(new ArrayList<String>());
-            return res;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            if (isPalindrome(s, i + 1)) {
-                for (List<String> list : partition(s.substring(i+1))) {
-                    list.add(0, s.substring(0, i + 1));
-                    res.add(list);
-                }
-            }
-        }
+ public static List < List < String >> partition(String s) {
+        List < List < String >> res = new ArrayList < > ();
+        List < String > path = new ArrayList < > ();
+        func(0, s, path, res);
         return res;
     }
-    
-    public boolean isPalindrome(String s, int n) {
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1))
+
+    static void func(int index, String s, List < String > path, List < List < String >> res) {
+        if (index == s.length()) {
+            res.add(new ArrayList < > (path));
+            return;
+        }
+        for (int i = index; i < s.length(); ++i) {
+            if (isPalindrome(s, index, i)) {
+                path.add(s.substring(index, i + 1));
+                func(i + 1, s, path, res);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    static boolean isPalindrome(String s, int start, int end) {
+        while (start <= end) {
+            if (s.charAt(start++) != s.charAt(end--))
                 return false;
         }
         return true;
