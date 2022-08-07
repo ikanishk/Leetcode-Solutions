bf0a1268dfef29be3254d766f9a4b1f8be33852a
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-08-07 15:20:42 +0530

    Time: 348 ms (48.84%), Space: 117.5 MB (7.15%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+    StringBuilder gy=new StringBuilder();
+        int I=0;
+    for(I=1; gy.length()<=b.length(); I++){
+        gy.append(a);
+        if(gy.toString().contains(b))return I;
+    }
+        if(gy.append(a).toString().contains(b))return I;
+        return -1;
+    }
+}
