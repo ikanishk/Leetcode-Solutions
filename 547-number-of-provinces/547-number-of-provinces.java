67aa696bfa7f2198a5963ea308b1589a191ed3e7
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-08-09 07:10:36 +0530

    Time: 4 ms (34.32%), Space: 53.4 MB (79.71%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,10 +1,17 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        //Initially convert the adjacency matrix to an adjacency List
         List<List<Integer>> adj = new ArrayList<>();
-        int n = isConnected.length;
-        for(int i=0;i<n;i++)
-            adj.add(new ArrayList());
         
+        //Calculate the length
+        int n = isConnected.length;
+    
+        //add an empty arraylist in front of every index
+        for(int i=0;i<n;i++){
+        adj.add(new ArrayList());
+        }
+        
+        //Mark every edge in the adjacency list.
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
@@ -15,8 +22,12 @@ class Solution {
                     adj.get(i).add(j);
             }
         }
+        
+        //The approach is to call the dfs a node, that dfs call will mark all its connections as visited.
+        //The moment we need to enter the if block means we have found a new province.
         int ans = 0;
         boolean vis[] = new boolean[n];
+        
         for(int i=0;i<n;i++)
         {
             if(vis[i] == false) 
