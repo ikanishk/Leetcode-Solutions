5155ad137c576d7c329c15cc36ad5b03b27edec7
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-08-12 04:52:26 +0530

    Time: 5 ms (79.37%), Space: 52 MB (26.67%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -13,52 +13,32 @@
  *     }
  * }
  */
-public class BSTIterator {
-    private Stack<TreeNode> stack = new Stack<TreeNode>();
-    boolean reverse = true; 
-    
-    public BSTIterator(TreeNode root, boolean isReverse) {
-        reverse = isReverse; 
-        pushAll(root);
-    }
-
-    /** @return whether we have a next smallest number */
-    public boolean hasNext() {
-        return !stack.isEmpty();
-    }
-
-    /** @return the next smallest number */
-    public int next() {
-        TreeNode tmpNode = stack.pop();
-        if(reverse == false) pushAll(tmpNode.right);
-        else pushAll(tmpNode.left); 
-        return tmpNode.val;
-    }
-    
-    private void pushAll(TreeNode node) {
-        while(node != null) {
-             stack.push(node);
-             if(reverse == true) {
-                 node = node.right; 
-             } else {
-                 node = node.left; 
-             }
-        }
-    }
-}
 class Solution {
+    List<Integer> lst = new ArrayList<Integer>();
     public boolean findTarget(TreeNode root, int k) {
-        if(root == null) return false; 
-        BSTIterator l = new BSTIterator(root, false); 
-        BSTIterator r = new BSTIterator(root, true); 
+        inorder(root);
+        int l = 0;
+        int r = lst.size() - 1;
         
-        int i = l.next(); 
-        int j = r.next(); 
-        while(i<j) {
-            if(i + j == k) return true; 
-            else if(i + j < k) i = l.next(); 
-            else j = r.next(); 
+        while(l<r){
+            int sum = lst.get(l) + lst.get(r);
+            if(sum == k){
+                return true;
+            }else if(sum < k){
+                l++;
+            }else{
+                r--;
+            }
         }
-        return false; 
+        return false;
+    }
+    
+    public void inorder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        lst.add(root.val);
+        inorder(root.right);
     }
 }
