1903b91a43d383a4e91027148a4136c7d26d0eb6
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-08-31 23:52:29 +0530

    Time: 75 ms (5.08%), Space: 111.9 MB (5.24%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,40 +1,42 @@
-
 class Solution {
-    public int numEnclaves(int[][] grid) {
-        for(int i=0; i < grid.length;i++) {
-            for(int j=0; j < grid[0].length;j++) {
-                //boundary's wale 1s ke connected components ko 'zero' karna hai.
-                if((i == 0 || i == grid.length-1 || j == 0 || j == grid[0].length-1) && grid[i][j] == 1) {
-                    dfs(grid,i,j);
-                } 
-            }
-        }
-        
-        
-        //remaining ones in the grid are enclaves
-        int count = 0;
-        for(int i=0; i < grid.length;i++) {
-            for(int j=0; j < grid[0].length;j++) {
-                if(grid[i][j] == 1) {
-                    count++;
-                }
-            }
-        }
-        
-        return count;
-    }
-    
-    
-    public void dfs(int[][]grid,int i,int j) {
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
-            return;
-        }
-        
-        grid[i][j] = 0;
-        
-        dfs(grid,i-1,j); //top
-        dfs(grid,i,j-1); //left
-        dfs(grid,i+1,j); //down
-        dfs(grid,i,j+1); //right
-    }
+	public int numEnclaves(int[][] grid) {
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (i == 0 || j == 0 || i == grid.length - 1 || j == grid[i].length - 1) {
+					if (grid[i][j] == 1)
+						bfs(i, j, grid);
+				}
+			}
+		}
+		int count = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (grid[i][j] == 1)
+					count++;
+			}
+		}
+		return count;
+
+	}
+
+	public static void bfs(int ni, int nj, int[][] grid) {
+		int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] { ni, nj });
+		boolean[][] visited = new boolean[grid.length][grid[0].length];
+		while (!q.isEmpty()) {
+			int[] r = q.poll();
+			grid[r[0]][r[1]] = 0;
+			if (visited[r[0]][r[1]])
+				continue;
+			visited[r[0]][r[1]] = true;
+			for (int d = 0; d < 4; d++) {
+				int i = r[0] + dirs[d][0];
+				int j = r[1] + dirs[d][1];
+				if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == 1 && !visited[i][j]) {
+					q.add(new int[] { i, j });
+				}
+			}
+		}
+	}
 }
