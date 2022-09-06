6a98f10c724f735e715bf6a207f5767c669cac6f
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-09-06 17:08:26 +0530

    Time: 8 ms (68.28%), Space: 64.8 MB (83.79%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.java
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -7,46 +7,33 @@
 
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        List<Integer> res = new ArrayList<Integer>();
-        int[] visited = new int[graph.length];
-        //0 -- unknown
-        //1 -- visiting
-        //2 -- safe
-        
-        for (int i = 0; i < graph.length; i++) {
-            if (isSafe(i, graph, visited)) {
-                res.add(i);
+        int[] safeNode = new int[graph.length];
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < graph.length; i++){
+            if(dfs(graph, i, safeNode)){
+               res.add(i);
             }
         }
-        
         return res;
+
     }
-    
-    private boolean isSafe(int index, int[][] graph, int[] visited) {
-        if (visited[index] == 2) {
-            //this vertex has been visited in previous path, and there is no cycle formed by this vertex
+
+    public boolean dfs(int[][] graph, int currentNode, int[] safeNode){
+        if(safeNode[currentNode] == 2){
             return true;
         }
-        
-        if (visited[index] == 1) {
-            //this vertex is visiting in current path, there exists a cycle formed by this vertex
+        if(safeNode[currentNode] == 1){
             return false;
         }
-        
-        //this vertex is on visiting path
-        visited[index] = 1;
-        int[] neighbor = graph[index];
-        
-        //visit all neighbours, if visit to itself again, there exists cycle
-        for (int i = 0; i < neighbor.length; i++) {
-            if (!isSafe(neighbor[i], graph, visited)) {
+
+        safeNode[currentNode] = 1;
+        int[] edge = graph[currentNode];
+        for(int i = 0; i < edge.length; i++){
+            if(!dfs(graph, edge[i], safeNode)){
                 return false;
             }
         }
-        
-        //after visiting all neighbours and there is no cycle, mark this vertex to be safe
-        visited[index] = 2;
+        safeNode[currentNode] = 2;
         return true;
     }
-
 }
