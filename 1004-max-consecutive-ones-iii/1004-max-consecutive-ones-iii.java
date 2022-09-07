bc19bf6f4858bffc2a10fe6de0f4a27998d766d8
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-09-07 19:29:21 +0530

    Time: 5 ms (40.41%), Space: 71.1 MB (45.73%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,28 +1,34 @@
 class Solution {
-    public int longestOnes(int[] nums, int k) {
-        if (nums == null) {
-            throw new IllegalArgumentException("Input array is null");
-        }
 
+    // TC : O(n)
+    // SC : O(1)
+    public int longestOnes(int[] nums, int k) {
         int start = 0;
         int end = 0;
-        int zeros = 0;
-        int maxCount = 0;
+        int maxLen = 0;
+        int flippedOnes = 0;
+        while(end<nums.length){
+            if(nums[end] ==1){
+                end++;
+            } else {
+                // nums[end] == 0
+                if(flippedOnes<k) {
+                    flippedOnes++;
+                    end++;
+                } else {
+                    // reduce some flips
+                    while(flippedOnes>=k) {
+                        if(nums[start] == 0) {
+                            flippedOnes--;
 
-        while (end < nums.length) {
-            if (nums[end] == 0) {
-                zeros++;
-            }
-            end++;
-            while (zeros > k) {
-                if (nums[start] == 0) {
-                    zeros--;
+                        }
+                         start++;
+                    }
                 }
-                start++;
             }
-            maxCount = Math.max(maxCount, end - start);
+           // System.out.println(end + " "+ start);
+            maxLen = Math.max(maxLen, end-start);
         }
-
-        return maxCount;
+        return maxLen;
     }
 }
