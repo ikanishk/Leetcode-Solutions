eee616e8d36864c3e661c2aa357022dbb420768e
Kumar Kanishk Pandey <71103596+ikanishk@users.noreply.github.com>
2022-09-13 23:25:32 +0530

    Time: 5 ms (38.56%), Space: 69.1 MB (11.61%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,3 +1,4 @@
+// Approach: We mark the maximum reachable index for every element and the moment we are on an index which in greater than the max reachable, we return false and if we are able to successfully parse the entire array and reach the last element, we return true;
 class Solution {
 public boolean canJump(int[] nums) {
     int reachable = 0;
